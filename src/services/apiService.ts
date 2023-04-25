563ae6c193032d680aa1d342e68ed56e3726cfa5
Frikar <ddglobatravel1@gmail.com>
2023-04-24 20:42:51 -0500

    Axios update

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-	baseURL: process.env.NEXTAPI_URL, // Reemplaza con la URL base de tu API
+	baseURL: "https://reminder.dvlance.com/api", // Reemplaza con la URL base de tu API
 	timeout: 3000,
-	httpsAgent: new (require('https').Agent)({ rejectUnauthorized: false }),
 });
